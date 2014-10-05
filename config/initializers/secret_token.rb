66c1f4a126eb0357b02b0bbc697a92926d9a5138
Kimberly Johnson <kimberly.k.johnson@gmail.com>
2014-10-05 16:34:35 -0400

    initial commit for the "Hello, Cloud Foundry!" project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hello::Application.config.secret_key_base = '0ee1d32e797c93c04101cfd06b13fc724b2f340a0f42e84f25f1f20233ad4d7cea822e4d36a0bbdb45fea68a8011eeac3f5b03a896c91345a8b887e15a7328e5'
